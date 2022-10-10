924b33867a4dd7cb266af0384a1f83932bd2b633
Nicolnegg <princesnegg@gmail.com>
2022-10-09 21:11:08 -0500

    Oauth GOOGLE

--- node/index.js
+++ node/index.js
@@ -5,6 +5,10 @@ import db from "./database/db.js"
 import router from "./routes/routes.js";
 import bcrypt from "bcryptjs";
 import session from "express-session";
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth20";
+
+
 
 import appRoutes from './routes/routes.js';
 
@@ -20,14 +24,69 @@ app.use('/usuario', appRoutes);
 dotenv.config({path:'./.env'});
 
 
+
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
-}));
+})); 
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use (router);
 
 app.listen(8000,()=>{
     console.log('Server UP running in http://localhost:8000')
+})
+
+
+
+//passport
+
+passport.use(
+    new GoogleStrategy({
+    clientID: '135583077973-8fku4sbr9chhk2julhfqa4a6bosnriom.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: 'http://localhost:8000/google/redirect'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        //done(null, profile); // passes the profile data to serializeUser
+        console.log(profile)
+
+        db.query('SELECT * FROM usuario WHERE cedula = ? ', [profile.id], async (error, results) => {
+
+        
+            if (Object.keys(results).length == 0) {
+                
+                db.query('INSERT INTO usuario SET ?', { cedula: profile.id, nombre: profile.name.givenName, apellido: profile.name.familyName }, async (error, resultados) => {
+                    if (error) throw error;
+                    console.log('Registrado google')
+                    console.log(resultados);
+                    console.log(db.query('SELECT * FROM usuario WHERE usuario_id = ? ', [resultados.insertId], async (error, results) => { 
+                        console.log(results[0].usuario_id) 
+                        done(null, results[0])
+                    }))
+                })
+                
+            }
+            else {
+                done(null, results[0])
+                console.log('ya registrado')
+                console.log(results[0].usuario_id)
+
+            }
+        })
+        
+    }
+));
+
+passport.serializeUser((user, done) =>{
+    done(null, user.usuario_id )
+})
+
+passport.deserializeUser((id, done) => {
+    db.query('SELECT * FROM usuario WHERE usuario_id = ? ', [id], async (error, results) => {
+        done(null, results[0])
+    })
 })
--- node/routes/routes.js
+++ node/routes/routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import passport from "passport";
 
 import * as ctrUser from "../controllers/PPController.js";
 
@@ -7,5 +8,10 @@ const router = express.Router();
 
 router.post("/registro", ctrUser.crearCuenta)
 router.post("/login", ctrUser.verificarUsuario)
+router.get("/google", passport.authenticate('google',{
+    scope: ['profile']
+}))  // vemos todos los perfiles de google
+router.get("/google/redirect", passport.authenticate('google'), (req, res) =>{res.send('estoo')} )
+router.get("/carrito-pago", ctrUser.pago_despues_carrito)
 
 export default router;
