876bcf1b94dcccf972ec3a1e2bb2e5dd1577f489
DR200131 <drubiano@unal.edu.co>
2022-10-19 22:08:55 -0500

    TodosProductos1

--- node/routes/routes.js
+++ node/routes/routes.js
@@ -4,6 +4,7 @@ import passport from "passport";
 import * as ctrUser from "../controllers/PPController.js";
 import * as menuUser from "../controllers/MenuGeneralController.js";
 import * as carritoUser from "../controllers/CarritoUsuario.js";
+import * as productoUser from "../controllers/ProductoUsuario.js";
 
 const router = express.Router();
 
@@ -16,9 +17,11 @@ router.get("/google", passport.authenticate('google',{
 router.get("/google/redirect", passport.authenticate('google'), (req, res) =>{res.send('estoo')} )
 router.get("/carrito-pago", ctrUser.pago_despues_carrito)
 router.get("/menu", menuUser.consultarEstablecimiento)
+router.get("/submenu", menuUser.consultarPAEstablecimiento)
 router.get('/logout',function(req,res){
     req.session.destroy();
 })
 router.get("/carrito", carritoUser.productosCarrito)
+router.get("/productos", productoUser.productosMenu)
 
 export default router;
