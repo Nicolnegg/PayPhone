c10bfc411b79e4ec5af46545dbd3bf9113b73b32
DR200131 <drubiano@unal.edu.co>
2022-11-16 20:18:01 -0500

    Arreglo crear ProductoUsuario

--- node/controllers/Est_Productos.js
+++ node/controllers/Est_Productos.js
@@ -1,19 +1,20 @@
 import connection from "../database/db.js";
+import Categoria from './../../front/src/components/buscar/Categoria';
 
-export async function agregarProduct(req, res){
+export async function agregarProducto(req, res){
     try {
         const nombre = req.body.nombre;
         const precio_venta = req.body.precio_venta;
         const cantidad = req.body.cantidad;
         const imagen = req.body.imagen;
+        const categoria = req.body.categoria;
         connection.query('SELECT * FROM producto WHERE nombre = ? ', [nombre], async (error, results) => {
             console.log(Object.keys(results).length);
             //revisar que el nombre sea diferente o si no ya creo el producto
             if (Object.keys(results).length == 0 ) {
                 //revisat id punto de atencion PREGUNTAR DIEGO
-                connection.query('INSERT INTO producto SET ?', { punto_atencion_id: xxxxxxxxxx, nombre: nombre, precio_venta: precio_venta, cantidad: cantidad, imagen: imagen},
+                connection.query('INSERT INTO producto SET ?', { punto_atencion_id: xxxxxxxxxx, nombre: nombre, precio_venta: precio_venta, cantidad: cantidad, imagen: imagen, categoria: categoria},
                         async (error, results) => {
-
                             if (error) {
                                 console.log(error);
                                 res.json({ isOK: false, msj: "Producto almacenado de forma INCORRECTA" })
@@ -29,5 +30,19 @@ export async function agregarProduct(req, res){
         })
     } catch (error) {
         res.json({ message: error.message })
+    }
+}
+
+export async function consultarCategorias(req,res){
+    try {
+        connection.query('SELECT * FROM categoria', async (error, results) => {
+                if (error) {
+                    res.json({ error })
+                } else {
+                    res.json({ isOK: true, msj: "Producto almacenado de forma correcta" })
+                }
+            })
+    } catch (error) {
+        
     }
 }
--- node/controllers/ProductoUsuario.js
+++ node/controllers/ProductoUsuario.js
@@ -14,5 +14,28 @@ export async function productosMenu(req, res) {
     } catch(error) {
         res.json({ message: error.message })
     }
+}
 
+export async function productosMenu(req, res) {
+    try{
+        const punto_atencion_id = req.body.punto_atencion_id;
+
+        connection.query('SELECT nombre, precio_venta, cantidad, imagen FROM producto WHERE activo = 1 AND punto_atencion_id = ?', [punto_atencion_id], async (error, results) => {
+            if (results == null){
+                res.json({ error })
+            }else{
+                res.json({ results })
+            }
+        })
+    } catch(error) {
+        res.json({ message: error.message })
+    }
+}
+
+export async function filtroProductos(req,res){
+    try {
+        const filtro = req.body.filtro;
+    } catch (error) {
+        
+    }
 }
