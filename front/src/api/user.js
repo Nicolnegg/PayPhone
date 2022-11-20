1f9e8719429675409134cdacd031dda868c5bd40
mpenaro <mpenaro@unal.edu.co>
2022-11-20 01:30:00 -0500

    Remove: duplicated function

--- front/src/api/user.js
+++ front/src/api/user.js
@@ -33,38 +33,6 @@ export function registro(data) {
   })
 }*/
 
-export function registroAdmin(data) {
-  const url = "http://localhost:8000/register/admin";
-  const params = {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  return fetch(url, params)
-    .then((response) => {
-      return response.json();
-    })
-    .then((result) => {
-      if (result.id) {
-        if (result.isAdmin) {
-          localStorage.setItem("ID", result.id);
-          return {
-            ok: true,
-            free: true,
-            message: "Admin creado correctamente",
-          };
-        } else {
-          return { ok: true, message: "Usuario creado correctamente" };
-        }
-      }
-      console.log("resultado:" + result);
-      return { ok: false, message: result.message };
-    });
-}
-
 export function iniciar_sesion(data) {
   const url = "http://localhost:8000/login";
   const params = {
