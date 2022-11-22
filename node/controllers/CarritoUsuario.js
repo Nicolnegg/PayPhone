3d4ef982dbd36e71b3ed4853d4cb70b412cce2b4
Nicolnegg <princesneg@gmail.com>
2022-11-21 22:46:15 -0500

    Productos Carrito

--- node/controllers/CarritoUsuario.js
+++ node/controllers/CarritoUsuario.js
@@ -1,29 +1,24 @@
 import connection from "../database/db.js";
 
-export async function productosCarrito(req, res){
-    req.session['productos'] = {'total': 0} 
-    console.log(req.session)
-    req.session['productos'] = {1:{'2productos': ''}} 
-    console.log(req.session)
-    req.session.productos[2] = { '222productos': '' } 
-    const total = 0;
-    console.log(req.session)
-    console.log(req.session.passport.user.cedula)
-    connection.query('SELECT * FROM carrito WHERE cedula = ? ', [req.session.passport.user.cedula], async (error, results) => {
+export async function getCarrito(req, res){
+    const usuario_id = req.body.usuario_id;
+    var total = 0;
+    req.session['productos'] = { total: 0 }
+
+    connection.query('SELECT P.nombre, P.precio_venta, C.cantidad FROM carrito C, producto P WHERE C.usuario_id = ? AND P.producto_id = C.producto_id', [usuario_id], async (error, results) => {
         if(results){
-            for (var i = 0; i < Object.keys(results).length; i++) {
-                connection.query('SELECT * FROM producto WHERE producto_id = ? ', [results[i].ID_producto], async (error, resultados) => {
-                    const foto  = resultados[0].imagen;
-                    const nombre = resultados[0].nombre;
-                    const precio = resultados[0].precio_venta;
-                    total += precio;
-                })
+            for(var j = 0; j < Object.keys(results).length; j++) {
+                total += results[j].precio_venta*results[j].cantidadP;
             }
-        }else{
-            console.log('nada en el carro')
+            res.json({ results,total});
+        }
+        else{
+            console.log('nada en el carro');
+            res.json({ isOK: false, msj: "Nada en el carro" });
         }
         
     })
+    
 }
 
 export async function agregarCarrito(req, res) {
@@ -32,17 +27,7 @@ export async function agregarCarrito(req, res) {
         const usuario_id = req.session.passport.user.usuario_id;
         const producto_id = req.body.producto_id;
         const cantidad = req.body.cantidad;
-        
-        connection.query('SELECT * FROM producto WHERE producto_id = ? ', [producto_id], async (error, resultados) => {
-            const foto = resultados[0].imagen;
-            const nombre = resultados[0].nombre;
-            const precio = resultados[0].precio_venta;
-            req.session.productos.total += precio * cantidad;
-            const esto = 'dos'
-            req.session.productos[esto] = { 'nombre': nombre, 'foto': foto, 'precio': precio, 'cantidad': cantidad };
-            console.log(req.session)
-            console.log(req.session)
-        })
+
         connection.query('INSERT INTO carrito SET ?', { usuario_id: usuario_id, producto_id: producto_id, cantidad: cantidad },
             async (error, results) => {
                 if (error) {
@@ -59,15 +44,15 @@ export async function agregarCarrito(req, res) {
     console.log(req.session)
 }
 
-export async function getCarrito(req, res) {
-    try {
-        const productosCarrito = req.session.productos
-        console.log("get carrito")
-        console.log(req.session)
-        console.log(productosCarrito)
-        res.json(productosCarrito)
-    } catch (error) {
-        res.json({ message: error.message })
+// export async function getCarrito(req, res) {
+//     try {
+//         const productosCarrito = req.session.productos
+//         console.log("get carrito")
+//         console.log(req.session)
+//         console.log(productosCarrito)
+//         res.json(productosCarrito)
+//     } catch (error) {
+//         res.json({ message: error.message })
 
-    }
-}
+//     }
+// }
