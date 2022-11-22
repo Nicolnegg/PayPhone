66945e3409fea9a56a61cf63f8a2eda3e3b542e7
mpenaro <mpenaro@unal.edu.co>
2022-11-22 09:13:26 -0500

    Add produco_id query get all products

--- node/controllers/ProductoUsuario.js
+++ node/controllers/ProductoUsuario.js
@@ -1,39 +1,51 @@
 import connection from "../database/db.js";
 
 export async function productosMenu(req, res) {
-    try{
-        const punto_atencion_id = req.params.punto_atencion_id;
+  try {
+    const punto_atencion_id = req.params.punto_atencion_id;
 
-        connection.query('SELECT nombre, precio_venta, cantidad, imagen FROM producto WHERE activo = 1 AND punto_atencion_id = ?', [punto_atencion_id], async (error, results) => {
-            if (results[0] == null){
-                res.json({ error })
-            }else{
-                res.json({ results })
-            }
-        })
-    } catch(error) {
-        res.json({ message: error.message })
-    }
+    connection.query(
+      "SELECT producto_id, nombre, precio_venta, cantidad, imagen FROM producto WHERE activo = 1 AND punto_atencion_id = ?",
+      [punto_atencion_id],
+      async (error, results) => {
+        if (results[0] == null) {
+          res.json({ error });
+        } else {
+          res.json({ results });
+        }
+      }
+    );
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 }
 
-export async function filtroProductos(req,res){
-    try {
-        const filtro = req.body.filtro;
-        connection.query('SELECT categoria_id FROM categoria WHERE nombre = ?', [filtro], async (error, results1) => {
-            if (results[0] == null){
-                res.json({ error })
-            }else{
-                connection.query('SELECT nombre, precio_venta, cantidad, imagen FROM producto WHERE activo = 1 AND punto_atencion_id = ? AND categoria_id = ?', [punto_atencion_id, results1[0]], async (error, results) => {
-                    if (results == null){
-                        res.json({ isOK: false, msj: "Este punto de atención no cuenta con productos de la categoria solicitada." })
-                    }else{
-                        res.json({ results })
-                    }
-                })
+export async function filtroProductos(req, res) {
+  try {
+    const filtro = req.body.filtro;
+    connection.query(
+      "SELECT categoria_id FROM categoria WHERE nombre = ?",
+      [filtro],
+      async (error, results1) => {
+        if (results[0] == null) {
+          res.json({ error });
+        } else {
+          connection.query(
+            "SELECT nombre, precio_venta, cantidad, imagen FROM producto WHERE activo = 1 AND punto_atencion_id = ? AND categoria_id = ?",
+            [punto_atencion_id, results1[0]],
+            async (error, results) => {
+              if (results == null) {
+                res.json({
+                  isOK: false,
+                  msj: "Este punto de atención no cuenta con productos de la categoria solicitada.",
+                });
+              } else {
+                res.json({ results });
+              }
             }
-        })
-
-    } catch (error) {
-        
-    }
+          );
+        }
+      }
+    );
+  } catch (error) {}
 }
