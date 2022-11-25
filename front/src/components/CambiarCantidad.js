2773ae2a37999323a1cbb607e997c33a740a6c8f
mpenaro <mpenaro@unal.edu.co>
2022-11-25 01:18:35 -0500

    Delete buttom

--- front/src/components/CambiarCantidad.js
+++ front/src/components/CambiarCantidad.js
@@ -1,54 +1,50 @@
-import {Button} from "react-bootstrap";
-import {React, useState} from 'react';
-import {cambiarCantidad} from "../api/canasta";
+import { Button } from "react-bootstrap";
+import { React, useState } from "react";
+import { cambiarCantidad } from "../api/canasta";
 
 const CambiarCantidad = (props) => {
+  const [data, setData] = useState({
+    cartItemId: props.id,
+    newQuantity: props.cantidad,
+  });
 
-    const[data ,setData] = useState({
-        cartItemId: props.id,
-        newQuantity: props.cantidad
-    })
-
-  const changeForm = e => {
-    if(e.target.name === 'newQuantity'){
-        setData({
-            ...data,
-            [e.target.name]: parseInt(e.target.value)
-        });
+  const changeForm = (e) => {
+    if (e.target.name === "newQuantity") {
+      setData({
+        ...data,
+        [e.target.name]: parseInt(e.target.value),
+      });
     }
-    
+
     //console.log(data)
-    };
+  };
 
-    function refreshPage() {
-        window.location.reload(false);
-    }
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
-    const nuevaCantidad = async () => {
-        const result = await cambiarCantidad(data);
-        refreshPage()
-    }
+  const nuevaCantidad = async () => {
+    const result = await cambiarCantidad(data);
+    refreshPage();
+  };
 
   return (
     <div>
-        <form className="d-flex">
+      <form className="d-flex">
         <div className=" form-floating mx-2" onChange={changeForm}>
-            <input 
-                type="number" 
-                id="newQuantity"
-                className="form-control"
-                name="newQuantity" 
-                min="1"
-                value = {data.newQuantity}
-                />
-            <label htmlFor="newQuantity">Cantidad</label>
+          <input
+            type="number"
+            id="newQuantity"
+            className="form-control"
+            name="newQuantity"
+            min="1"
+            value={data.newQuantity}
+          />
+          <label htmlFor="newQuantity">Cantidad</label>
         </div>
-        <Button className="btn mb-3 mt-2 me-2 fw-bold" onClick={() => nuevaCantidad()}>
-            Cambiar
-        </Button>
-    </form>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default CambiarCantidad
+export default CambiarCantidad;
