5532235d1da02701027343cf91440e7fdbd691ff
mpenaro <mpenaro@unal.edu.co>
2022-11-25 02:22:44 -0500

    Fix read data

--- front/src/views/Recomendaciones.jsx
+++ front/src/views/Recomendaciones.jsx
@@ -1,126 +1,126 @@
-import {React, useState, useEffect} from 'react'
-import { buscarRecomendaciones } from '../api/buscar'
-import ModalAnuncio from '../components/ModalAnuncio'
-import Table from 'react-bootstrap/Table';
+import { React, useState, useEffect } from "react";
+import { buscarRecomendaciones } from "../api/buscar";
+import ModalAnuncio from "../components/ModalAnuncio";
+import Table from "react-bootstrap/Table";
 import jwtDecode from "jwt-decode";
-import {getAccessToken} from "../api/auth";
-import {verOrdenes} from "../api/orders";
-import PostsRelacionados from "../components/PostsRelacionados"
-
+import { getAccessToken } from "../api/auth";
+import { verOrdenes } from "../api/orders";
+import PostsRelacionados from "../components/PostsRelacionados";
 
 const Recomendaciones = () => {
+  const [recomendacion, setRecomendacion] = useState([]);
 
-  const[recomendacion, setRecomendacion] = useState([])
-
-    const UserId = jwtDecode(getAccessToken()).id;
-    const[ordenes, setOrdenes] = useState([])
-
-    useEffect(() => {
-        verOrdenes(UserId).then(response => {
-            setOrdenes(response);
-        })
-    }, [])
+  const UserId = jwtDecode(getAccessToken()).id;
+  const [ordenes, setOrdenes] = useState([]);
 
   useEffect(() => {
-    buscarRecomendaciones().then(response => {
-      setRecomendacion(response);
-    })
-  }, [])
+    verOrdenes(UserId).then((response) => {
+      setOrdenes(response.results);
+    });
+  }, []);
 
-    function validaOrdenes(ordenes){
-        if (ordenes.length === 0){
-            return (
-                <p className="welcome1 fw-bold text-center">Aún no has realizado compras.</p>
-            )
-        }
-    }
+  useEffect(() => {
+    buscarRecomendaciones().then((response) => {
+      setRecomendacion(response.results);
+    });
+  }, []);
 
-    function validaRecomendados(ordenes,orden,item){
-        let diferencias = 0;
-        let total = 0;
-        const revisa = ordenes.slice(0,ordenes.indexOf(orden))
-        if (revisa.length > 0){
-            revisa.forEach(previo => {
-                previo.OrderItems.forEach(checkItem => {
-                    if (item.Post.PostTitle !== checkItem.Post.PostTitle){
-                        diferencias++;
-                    }
-                    total++;
-                })
-            })
-        }
-        if (diferencias === total) {
-            return (
-                <div>
-                    <h5 className="mb-3">Porque compraste <b className="mid-badge">{item.Post.PostTitle}</b>, te
-                        recomendamos:</h5>
-                    <PostsRelacionados id={item.PostId}/>
-                </div>
-            )
-        }
+  function validaOrdenes(ordenes) {
+    if (ordenes.length === 0) {
+      return (
+        <p className="welcome1 fw-bold text-center">
+          Aún no has realizado compras.
+        </p>
+      );
     }
+  }
+
+  function validaRecomendados(ordenes, orden, item) {
+    let diferencias = 0;
+    let total = 0;
+    const revisa = ordenes.slice(0, ordenes.indexOf(orden));
+    if (revisa.length > 0) {
+      revisa.forEach((previo) => {
+        previo.OrderItems.forEach((checkItem) => {
+          if (item.Post.PostTitle !== checkItem.Post.PostTitle) {
+            diferencias++;
+          }
+          total++;
+        });
+      });
+    }
+    if (diferencias === total) {
+      return (
+        <div>
+          <h5 className="mb-3">
+            Porque compraste <b className="mid-badge">{item.Post.PostTitle}</b>,
+            te recomendamos:
+          </h5>
+          <PostsRelacionados id={item.PostId} />
+        </div>
+      );
+    }
+  }
 
   return (
-      <div className="contenedorPerfil text-center d-flex">
-        <div className="container rounded row w-100">
-            <div className="welcome2">
-                <h5 className="text-center mt-1 mb-3 fw-bold yellow">Los elegidos de payphone</h5>
-                <div className="d-flex align-items-center justify-content-center">
-                    <Table hover variant="light">
-                        <thead>
-                        <tr className="table-primary welcome">
-                            <th colSpan="2" className="text-center">Anuncio</th>
-                            <th>Precio</th>
-                            <th className="text-center">Descripción</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody className="align-middle">
-                        {
-                            recomendacion.map((recomendacion)=>(
-                                <tr>
-                                    <td>
-                                        <svg
-                                            className="bd-placeholder-img rounded" width="160" height="90" role="img">
-                                            <title>Placeholder</title>
-                                            <rect width="100%" height="100%" fill="#eee"></rect>
-                                            <text x="50%" y="50%" fill="#aaa" dy=".3em">Imagen</text>
-                                        </svg>
-                                    </td>
-                                    <td>
-                                        {recomendacion.postTitle}
-                                    </td>
-                                    <td>
-                                        ${recomendacion.postPrice}
-                                    </td>
-                                    <td>
-                                        {recomendacion.postDescription}
-                                    </td>
-                                    <td>
-                                        <ModalAnuncio anuncio={recomendacion} />
-                                    </td>
-                                </tr>))
-                        }
-                        </tbody>
-                    </Table>
-                </div>
-            </div>
-            <div>
-                <h5 className="text-center mt-4 mb-3 fw-bold welcome">Para ti</h5>
-                {
-                    ordenes.map((orden)=>(
-                        orden.OrderItems.map((item)=>(
-                            validaRecomendados(ordenes,orden,item)
-                        ))
-                    ))
-                }
-                {
-                    validaOrdenes(ordenes)
-                }
-            </div>
+    <div className="contenedorPerfil text-center d-flex">
+      <div className="container rounded row w-100">
+        <div className="welcome2">
+          <h5 className="text-center mt-1 mb-3 fw-bold yellow">
+            Los elegidos de payphone
+          </h5>
+          <div className="d-flex align-items-center justify-content-center">
+            <Table hover variant="light">
+              <thead>
+                <tr className="table-primary welcome">
+                  <th colSpan="2" className="text-center">
+                    Anuncio
+                  </th>
+                  <th>Precio</th>
+                  <th className="text-center">Descripción</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody className="align-middle">
+                {recomendacion.map((recomendacion) => (
+                  <tr>
+                    <td>
+                      <picture>
+                        <img
+                          src={recomendacion.imagen}
+                          alt={recomendacion.nombre}
+                          style={{
+                            width: "5rem",
+                            maxWidth: "100%",
+                            objectFit: "cover",
+                          }}
+                        />
+                      </picture>
+                    </td>
+                    <td>{recomendacion.nombre}</td>
+                    <td>${recomendacion.precio_venta}</td>
+                    <td>{recomendacion.nombre}</td>
+                    <td>
+                      <ModalAnuncio anuncio={recomendacion} />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
         </div>
+        {/* <div>
+          <h5 className="text-center mt-4 mb-3 fw-bold welcome">Para ti</h5>
+          {ordenes.map((orden) =>
+            orden.OrderItems.map((item) =>
+              validaRecomendados(ordenes, orden, item)
+            )
+          )}
+          {validaOrdenes(ordenes)}
+        </div> */}
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Recomendaciones
+export default Recomendaciones;
