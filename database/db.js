fa65bc878fe6ffd357f00d277a8ee9e76ff0d8fa
DR200131 <drubiano@unal.edu.co>
2022-11-26 00:00:33 -0500

    Update

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ import { Sequelize } from "sequelize";
 
 const connection = mysql.createConnection({
     host: "us-cdbr-east-06.cleardb.net",
-    user: "b5d24b42e41a1a",
-    password: "c1ada6da",
-    database: "heroku_b4ea2cfb2e15836"
+    user: "b7677a191aa0ad",
+    password: "46abfd86",
+    database: "heroku_1a8c3f63a1a0d85"
 });
 
 connection.connect((error) => {
